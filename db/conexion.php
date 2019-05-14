fab3d7bc48e911920a98c9bd69d40439b268f9bd
camilo <cacomas@uninorte.edu.co>
2019-05-14 00:17:22 -0500

    third commit

--- db/conexion.php
+++ db/conexion.php
@@ -1,13 +1,13 @@
-<?php 
-class database  
+<?php
+class database
 {
     public static  function conectar()
     {
-        $db= new mysqli("localhost","root","","superatedatabase");
+        $db= new mysqli("sql211.epizy.com","epiz_23904845","DjARsvw2h","epiz_23904845_superatedatabase");
         $r=$db->query("set names 'utf8' ");
-        return $db;    
+        return $db;
     }
-   
+
 }
 
 
